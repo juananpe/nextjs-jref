d215b576317e40a9eb3a7b1d841776f6937d3f98
Juanan Pereira <juanan.pereira@ehu.eus>
2023-08-04 12:02:57 +0200

    Update prod domain fetching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,11 +1,14 @@
+import getDomain from "@/app/lib/getDomain";
+
 async function getData() {
-    // const endpoint = "http://localhosts:3000/api/posts";
-    // const res = await fetch(endpoint);
-    // if (!res.ok) {
-    //     throw new Error("Failed to fetch API");
-    // }
-    // return res.json();
-    return {items: [] };
+    const domain = getDomain();
+    const endpoint = `${domain}/api/posts`;
+    const res = await fetch(endpoint);
+    if (!res.ok) {
+        throw new Error("Failed to fetch API");
+    }
+    return res.json();
+    
 }
 
 
